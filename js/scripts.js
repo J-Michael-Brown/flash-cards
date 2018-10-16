6eb369da984d1ebd13e3b4856b9a3db11f8993ea
Michael Brown and Katrina Hockman <michael@>
2018-10-16 13:53:54 -0700

    Add styles in css, add some content, complete rows layout.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,79 @@
- $(document).ready(function() {
-   $("#first-subject").click(function() {
-     $("#first-info").toggle();
-   });
+$(document).ready(function() {
+  $("#1st-subject").click(function() {
+    $("#1st-info").toggle();
+  });
 
-   $("#second-subject").click(function() {
-     $("#second-info").toggle();
-   });
+  $("#2nd-subject").click(function() {
+    $("#2nd-info").toggle();
+  });
 
- });
+  $("#3rd-subject").click(function() {
+    $("#3rd-info").toggle();
+  });
+
+  $("#4th-subject").click(function() {
+    $("#4th-info").toggle();
+  });
+
+  $("#5th-subject").click(function() {
+    $("#5th-info").toggle();
+  });
+
+  $("#6th-subject").click(function() {
+    $("#6th-info").toggle();
+  });
+
+  $("#7th-subject").click(function() {
+    $("#7th-info").toggle();
+  });
+
+  $("#8th-subject").click(function() {
+    $("#8th-info").toggle();
+  });
+
+  $("#9th-subject").click(function() {
+    $("#9th-info").toggle();
+  });
+
+  $("#10th-subject").click(function() {
+    $("#10th-info").toggle();
+  });
+
+  $("#11th-subject").click(function() {
+    $("#11th-info").toggle();
+  });
+
+  $("#12th-subject").click(function() {
+    $("#12th-info").toggle();
+  });
+
+  $("#13th-subject").click(function() {
+    $("#13th-info").toggle();
+  });
+
+  $("#14th-subject").click(function() {
+    $("#14th-info").toggle();
+  });
+
+  $("#15th-subject").click(function() {
+    $("#15th-info").toggle();
+  });
+
+  $("#16th-subject").click(function() {
+    $("#16th-info").toggle();
+  });
+
+  $("#17th-subject").click(function() {
+    $("#17th-info").toggle();
+  });
+
+  $("#18th-subject").click(function() {
+    $("#18th-info").toggle();
+  });
+
+  $("#19th-subject").click(function() {
+    $("#19th-info").toggle();
+  });
+
+
+});
