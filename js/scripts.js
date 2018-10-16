b5dace29ef43cfa4fdbfc6251b62874a950087c9
Michael Brown and Katrina Hockman <michael@>
2018-10-16 16:55:25 -0700

    change toggle to slideToggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,78 @@
 $(document).ready(function() {
   $("#1st-subject").click(function() {
-    $("#1st-info").toggle();
+    $("#1st-info").slideToggle();
   });
 
   $("#2nd-subject").click(function() {
-    $("#2nd-info").toggle();
+    $("#2nd-info").slideToggle();
   });
 
   $("#3rd-subject").click(function() {
-    $("#3rd-info").toggle();
+    $("#3rd-info").slideToggle();
   });
 
   $("#4th-subject").click(function() {
-    $("#4th-info").toggle();
+    $("#4th-info").slideToggle();
   });
 
   $("#5th-subject").click(function() {
-    $("#5th-info").toggle();
+    $("#5th-info").slideToggle();
   });
 
   $("#6th-subject").click(function() {
-    $("#6th-info").toggle();
+    $("#6th-info").slideToggle();
   });
 
   $("#7th-subject").click(function() {
-    $("#7th-info").toggle();
+    $("#7th-info").slideToggle();
   });
 
   $("#8th-subject").click(function() {
-    $("#8th-info").toggle();
+    $("#8th-info").slideToggle();
   });
 
   $("#9th-subject").click(function() {
-    $("#9th-info").toggle();
+    $("#9th-info").slideToggle();
   });
 
   $("#10th-subject").click(function() {
-    $("#10th-info").toggle();
+    $("#10th-info").slideToggle();
   });
 
   $("#11th-subject").click(function() {
-    $("#11th-info").toggle();
+    $("#11th-info").slideToggle();
   });
 
   $("#12th-subject").click(function() {
-    $("#12th-info").toggle();
+    $("#12th-info").slideToggle();
   });
 
   $("#13th-subject").click(function() {
-    $("#13th-info").toggle();
+    $("#13th-info").slideToggle();
   });
 
   $("#14th-subject").click(function() {
-    $("#14th-info").toggle();
+    $("#14th-info").slideToggle();
   });
 
   $("#15th-subject").click(function() {
-    $("#15th-info").toggle();
+    $("#15th-info").slideToggle();
   });
 
   $("#16th-subject").click(function() {
-    $("#16th-info").toggle();
+    $("#16th-info").slideToggle();
   });
 
   $("#17th-subject").click(function() {
-    $("#17th-info").toggle();
+    $("#17th-info").slideToggle();
   });
 
   $("#18th-subject").click(function() {
-    $("#18th-info").toggle();
+    $("#18th-info").slideToggle();
   });
 
   $("#19th-subject").click(function() {
-    $("#19th-info").toggle();
+    $("#19th-info").slideToggle();
   });
 
 
